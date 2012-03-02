b1c716c1a61c97aa7581f4c34944ce2bc8b39b12
Will Wright <willjwright@mac.com>
2012-03-01 20:28:38 -0800

    Updated to 1.1-r5 - Yet to be tested

--- src/tk/tyzoid/plugins/ChestTrap/Listeners/BListener.java
+++ src/tk/tyzoid/plugins/ChestTrap/Listeners/BListener.java
@@ -2,12 +2,15 @@ package tk.tyzoid.plugins.ChestTrap.Listeners;
 
 //import org.bukkit.entity.Player;
 //import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
 
 import tk.tyzoid.plugins.ChestTrap.ChestTrap;
 //import tk.tyzoid.plugins.ChestTrap.Lib.Coordinate;
 
-public class BListener  extends BlockListener{
+public class BListener implements Listener {
 	private final ChestTrap plugin;
 	String pluginname;
 	
@@ -17,7 +20,7 @@ public class BListener  extends BlockListener{
         
         pluginname = plugin.pluginname;
     }
-	
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onBlockBreak(BlockBreakEvent event){
 		if(event.isCancelled()){
 			return;
